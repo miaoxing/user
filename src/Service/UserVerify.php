d6fd0a0b1d9f25b44b13a15d155d8cb79db94878
twinh <twinhuang@qq.com>
2017-02-21 10:35:06 +0800

    控制验证链接只能访问一次, 移除重复的角色操作

--- src/Service/UserVerify.php
+++ src/Service/UserVerify.php
@@ -6,6 +6,8 @@ use miaoxing\plugin\BaseService;
 
 class UserVerify extends BaseService
 {
+    const EXPIRE = 86400;
+
     public function generate($user)
     {
         $nonce = wei()->random->string(32);
@@ -26,7 +28,7 @@ class UserVerify extends BaseService
     {
         // 1. 超时判断
         $timestamp = isset($data['timestamp']) ? $data['timestamp'] : 0;
-        if ($timestamp < time() - 86400) {
+        if ($timestamp < time() - static::EXPIRE) {
             return ['code' => -1, 'message' => '链接超时无效'];
         }
 
@@ -38,11 +40,16 @@ class UserVerify extends BaseService
         }
         $password = $user['password'];
 
-        // 3. 验证用户
+        // 3. 验证签名是否正确
         $nonce = $data['nonce'] ?: '';
         $sign = md5($data['userId'] . $password . $timestamp . $nonce);
         if ($sign != $data['sign']) {
             return ['code' => -3, 'message' => '链接验证无效'];
+        }
+
+        // 4. 验证码链接是否被用过
+        if (!wei()->cache->add('user:verify:' . $sign, true, static::EXPIRE)) {
+            return ['code' => -4, 'message' => '链接被访问过'];
         }
 
         return ['code' => 1, 'message' => '验证成功', 'user' => $user];
