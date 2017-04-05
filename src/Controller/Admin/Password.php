bc08053ac83ac12f39f4c63d2a5d581bad9a797e
twinh <twinhuang@qq.com>
2017-04-05 04:27:14 +0000

    Fix coding standard [skip fix]

--- src/Controller/Admin/Password.php
+++ src/Controller/Admin/Password.php
@@ -9,7 +9,7 @@ class Password extends BaseController
     protected $controllerName = '密码管理';
 
     protected $actionPermissions = [
-        'index,update' => '修改密码'
+        'index,update' => '修改密码',
     ];
 
     public function indexAction($req)
