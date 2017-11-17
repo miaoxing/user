54d12f0ea5dbefb53089929e0c58c702c799d666
twinh <twinhuang@qq.com>
2017-11-17 16:14:25 +0800

    移除多余内容

--- resources/views/admin/groups/index.php
+++ resources/views/admin/groups/index.php
@@ -1,15 +1,7 @@
 <?php
 
 $view->layout();
-
 $wei->page->addActionJs('index');
 ?>
-
-<?= $block('header-actions') ?>
-<a class="btn btn-white" id="sync-from-wechat">
-  <i class="fa fa-refresh"></i> 从微信同步分组
-</a>
-<a class="btn btn-success" href="<?= $url('admin/groups/new') ?>">添加用户组</a>
-<?= $block->end() ?>
 
 <div id="root"></div>
