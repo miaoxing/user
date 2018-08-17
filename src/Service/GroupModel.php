bbd7569a982ea84ba48eaf9ccad029a99b5993f2
twinh <twinhuang@qq.com>
2018-08-17 11:20:25 +0800

    feat: 增加parentId调用

--- src/Service/GroupModel.php
+++ src/Service/GroupModel.php
@@ -7,12 +7,16 @@ use Miaoxing\Plugin\Model\GetSetTrait;
 use Miaoxing\Plugin\Model\QuickQueryTrait;
 use Miaoxing\Plugin\Model\SoftDeleteTrait;
 use Miaoxing\Plugin\Service\Group;
+use Miaoxing\User\Metadata\GroupTrait;
 
 /**
  * GroupModel
+ *
+ * @property GroupModel $parent
  */
 class GroupModel extends Group
 {
+    use GroupTrait;
     use CastTrait;
     use QuickQueryTrait;
     use GetSetTrait;
@@ -21,4 +25,9 @@ class GroupModel extends Group
     protected $deletedAtColumn = 'deleteTime';
 
     protected $deletedByColumn = 'deleteUser';
+
+    public function parent()
+    {
+        return $this->hasOne(wei()->groupModel(), 'parentId', 'id');
+    }
 }
