512a1c4212e37945f563b7ca6f20f277d15f8181
twinh <twinhuang@qq.com>
2018-09-09 12:09:02 +0800

    feat: 增加旧版用户弹出层的支持

---
+++ resources/components/UserPopoverV1.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import rp from "require-promise";
+import Loadable from 'react-loadable';
+import Loading from "components/Loading";
+import app2 from "app2";
+
+const loader = rp('template');
+
+const UserPopoverV1 = ({data}) => {
+  const UserPopover = () => <div dangerouslySetInnerHTML={{__html: template.render('user-info-tpl', data)}}/>;
+
+  const LoadableComponent = Loadable({
+    loader: () => loader.then(() => UserPopover),
+    loading: Loading,
+  });
+  return <LoadableComponent/>
+};
+
+export default UserPopoverV1;
