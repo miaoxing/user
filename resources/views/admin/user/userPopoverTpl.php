08b90ad44b3b2d9d06ab9408e131310ac54fb937
twinh <twinhuang@qq.com>
2018-10-16 14:20:09 +0800

    feat: 控制显示标签和分组

--- resources/views/admin/user/userPopoverTpl.php
+++ resources/views/admin/user/userPopoverTpl.php
@@ -23,18 +23,21 @@
     <i class="js-user-tooltip fa fa-lock text-success" title="已锁定,不会被同步为外部的地区,如微信的资料"></i>
     <% } %>
   </div>
-  <div>
-    分组：<select class="update-group" data-id="<%= id %>">
-      <option value="0"><?= $setting('user.titleDefaultGroup') ?: '未分组' ?></option>
-      <?php foreach ($wei->group()->findAll() as $group) : ?>
-        <option value="<?= $group['id'] ?>"><?= $group['name'] ?></option>
-      <?php endforeach ?>
-    </select>
-  </div>
-  <div>
-    标签：<%= tagName ? (tagName + ' ') : '' %><a class="js-user-edit-tags" data-id="<%= id %>"
-      data-tag-ids="<%= tagIds %>">编辑</a>
-  </div>
+  <?php if (wei()->plugin->isInstalled('user-tag')) { ?>
+    <div>
+      标签：<%= tagName ? (tagName + ' ') : '' %><a class="js-user-edit-tags" data-id="<%= id %>"
+        data-tag-ids="<%= tagIds %>">编辑</a>
+    </div>
+  <?php } else { ?>
+    <div>
+      分组：<select class="update-group" data-id="<%= id %>">
+        <option value="0"><?= $setting('user.titleDefaultGroup') ?: '未分组' ?></option>
+        <?php foreach ($wei->group()->findAll() as $group) : ?>
+          <option value="<?= $group['id'] ?>"><?= $group['name'] ?></option>
+        <?php endforeach ?>
+      </select>
+    </div>
+  <?php } ?>
   <?php $event->trigger('adminUserPopover') ?>
   <div>
     操作：<a href="<%= $.url('admin/user/userinfo', {id: id}) %>" target="_blank">编辑</a>
