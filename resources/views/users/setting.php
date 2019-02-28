9f44dc7601011bdd3794a25818fb5442ef951465
twinh <twinhuang@qq.com>
2019-02-28 12:03:28 +0800

    refactoring: .list-heading => .list-title, .list-body => .list-text

--- resources/views/users/setting.php
+++ resources/views/users/setting.php
@@ -3,26 +3,26 @@
 <ul class="list list-indented">
   <li class="list-item-link">
     <a href="<?= $url('users/edit') ?>" class="list-item">
-      <h4 class="list-heading">
+      <h4 class="list-title">
         个人资料
       </h4>
     </a>
   </li>
   <li class="  list-item list-item-divider">
-    <h3 class="list-heading">
+    <h3 class="list-title">
 
     </h3>
   </li>
   <li class="list-item-link">
     <a href="<?= $url('password') ?>" class="list-item">
-      <h4 class="list-heading">
+      <h4 class="list-title">
         修改密码
       </h4>
     </a>
   </li>
   <li class="list-item-link">
     <a href="<?= $url('password/reset') ?>" class="list-item">
-      <h4 class="list-heading">
+      <h4 class="list-title">
         忘记密码
       </h4>
     </a>
