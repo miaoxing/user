95e57f126928cb741cf4741d0e7bae5f972bb048
twinh <twinhuang@qq.com>
2020-03-04 17:26:49 +0800

    refactor: ConfigTrait 作为通用功能，移到 services 中

--- src/Service/Group.php
+++ src/Service/Group.php
@@ -2,7 +2,7 @@
 
 namespace Miaoxing\User\Service;
 
-use Miaoxing\Config\ConfigTrait;
+use Miaoxing\Services\ConfigTrait;
 use Miaoxing\Services\Model\ReqQueryTrait;
 use Miaoxing\User\Service\GroupModel;
 
