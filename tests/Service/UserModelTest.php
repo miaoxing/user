bc7af03cfc2eb61431cd9f9f0ea49fd200243957
twinh <twinhuang@qq.com>
2020-04-01 22:21:45 +0800

    test: 用户测试更新

--- tests/Service/UserModelTest.php
+++ tests/Service/UserModelTest.php
@@ -2,35 +2,77 @@
 
 namespace MiaoxingTest\User\Service;
 
+use Miaoxing\Plugin\Service\User;
+use Miaoxing\Plugin\Service\UserModel;
+
 class UserModelTest extends \Miaoxing\Plugin\Test\BaseTestCase
 {
-    public function setUp(): void
+    /**
+     * 测试获取昵称
+     */
+    public function testGetNickName()
     {
-        parent::setUp();
-        wei()->user()->delete();
-        wei()->user()
-            ->setPlainPassword('admin')
-            ->save([
-                'id' => 1,
-                'username' => 'miaostar',
-                'email' => '790449591@qq.com',
-                'mobile' => '15989130451',
-                'admin' => true,
-            ]);
+        $user = UserModel::new();
+        $this->assertEquals('', $user->getNickName());
 
-        // 创建未启用的用户
-        wei()->user()->save([
-            'id' => 2,
-            'username' => 'admin-not-enable',
-            'enable' => false,
-        ]);
+        $this->assertEquals('', $user->getNickName());
+
+        $user['name'] = 'name';
+        $this->assertEquals('name', $user->getNickName());
+
+        $user['username'] = 'username';
+        $this->assertEquals('username', $user->getNickName());
+
+        $user['nickName'] = 'nickName';
+        $this->assertEquals('nickName', $user->getNickName());
+    }
+
+    public function testIsAdmin()
+    {
+        $user = UserModel::new();
+        $this->assertFalse($user->isAdmin());
+
+        $user->admin = true;
+        $this->assertTrue($user->isAdmin());
     }
 
     public function testUpdateGroup()
     {
-        $user = wei()->user();
-        $user->updateGroup(1);
+        $user = UserModel::new();
 
-        $this->assertEquals(1, $user['groupId']);
+        $ret = $user->updateGroup(1);
+        $this->assertRetSuc($ret);
+
+        $this->assertEquals(1, $user->groupId);
+    }
+
+    public function testAvatar()
+    {
+        $user = UserModel::new();
+        $this->assertEquals(User::cur()->defaultAvatar, $user->avatar);
+
+        $user->avatar = 'test.jpg';
+        $this->assertEquals('test.jpg', $user->avatar);
+    }
+
+    /**
+     * 检查手机号码能否绑定
+     */
+    public function testCheckMobile()
+    {
+        $mobile = '12800138000';
+        UserModel::delete('mobile', $mobile);
+
+        $testUser = UserModel::new();
+        $ret = $testUser->checkMobile($mobile);
+        $this->assertRetSuc($ret, '手机号码可以绑定', '未存在指定手机号码的用户,可以绑定');
+
+        $user = UserModel::save(['mobile' => $mobile]);
+        $ret = $testUser->checkMobile($mobile);
+        $this->assertRetSuc($ret, '手机号码可以绑定');
+
+        $user->setMobileVerified()->save();
+        $ret = $testUser->checkMobile($mobile);
+        $this->assertRetErr($ret, -1, '已存在认证该手机号码的用户');
     }
 }
