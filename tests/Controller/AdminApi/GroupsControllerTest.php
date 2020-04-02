43b75b324d21c4a8d45809e97585b7154da161bf
twinh <twinhuang@qq.com>
2020-04-02 11:58:46 +0800

    test: 增加自定义 action 测试

--- tests/Controller/AdminApi/GroupsControllerTest.php
+++ tests/Controller/AdminApi/GroupsControllerTest.php
@@ -3,7 +3,18 @@
 namespace MiaoxingTest\User\Controller\AdminApi;
 
 use Miaoxing\Plugin\Test\BaseControllerTestCase;
+use Miaoxing\User\Service\GroupModel;
 
 class GroupsControllerTest extends BaseControllerTestCase
 {
+    public function testCreateAction()
+    {
+        $name = '测试分组' . time();
+
+        $ret = $this->visitCurPage(['name' => $name])->login()->json()->response();
+        $this->assertRetSuc($ret);
+
+        $group = GroupModel::desc('id')->first();
+        $this->assertSame($name, $group->name);
+    }
 }
