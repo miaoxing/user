71c05a60c107e0d2957412b4624d7db415b6bcdc
twinh <twinhuang@qq.com>
2020-04-02 11:45:11 +0800

    test: 调通控制器默认测试

--- tests/Controller/Admin/GroupsTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace MiaoxingTest\User\Controller\Admin;
-
-class GroupsTest extends \Miaoxing\Plugin\Test\BaseControllerTestCase
-{
-}
---
+++ tests/Controller/AdminApi/GroupsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MiaoxingTest\User\Controller\AdminApi;
+
+use Miaoxing\Plugin\Test\BaseControllerTestCase;
+
+class GroupsControllerTest extends BaseControllerTestCase
+{
+}
