47320284542dff7b308c64e95c968b4815c85eee
twinh <twinhuang@qq.com>
2020-09-22 15:38:05 +0800

    test: 增加查询用户列表测试

--- tests/pages/admin-api/users/IndexTest.php
+++ tests/pages/admin-api/users/IndexTest.php
@@ -2,10 +2,13 @@
 
 namespace MiaoxingTest\User\Pages\AdminApi\Users;
 
+use Carbon\Carbon;
 use Miaoxing\Plugin\Service\Tester;
 use Miaoxing\Plugin\Service\User;
 use Miaoxing\Plugin\Test\BaseTestCase;
+use Miaoxing\Services\Service\SexConst;
 use Miaoxing\User\Service\UserModel;
+use Wei\Time;
 
 class IndexTest extends BaseTestCase
 {
@@ -17,8 +20,41 @@ class IndexTest extends BaseTestCase
             'name' => '测试' . time(),
         ]);
 
-        $ret = Tester::request(['id' => $user->id])->getAdminApi('users');
+        $ret = Tester::getAdminApi('users');
 
         $this->assertSame($user->name, $ret['data'][0]['name']);
+    }
+
+    public function testGetSearch()
+    {
+        User::loginById(1);
+
+        $user = UserModel::save([
+            'name' => '测试' . time(),
+            'nickName' => 'nickName',
+            'mobile' => '13800138000',
+            'sex' => SexConst::SEX_FEMALE,
+            'country' => '中国',
+            'province' => '广东',
+            'city' => '深圳',
+        ]);
+
+        $request = [
+            'name$ct' => $user->name,
+            'nickName$ct' => $user->nickName,
+            'sex' => $user->sex,
+            'country' => $user->country,
+            'province' => $user->province,
+            'city' => $user->city,
+            'createdAtMin' => (string) Carbon::now()->subMinute(),
+            'createdAtMax' => Time::now(),
+        ];
+
+        $ret = Tester::request($request)->getAdminApi('users');
+        $this->assertSame($user->name, $ret['data'][0]['name']);
+
+        $request['createdAtMin'] = (string) Carbon::now()->addMinute();
+        $ret = Tester::request($request)->getAdminApi('users');
+        $this->assertEmpty($ret['data']);
     }
 }
