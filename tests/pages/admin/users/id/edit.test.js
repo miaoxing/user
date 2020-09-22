bf5f00980ab0a6eaebcd18afea55480fcf57aeec
twinh <twinhuang@qq.com>
2020-09-22 18:23:44 +0800

    test: 增加前端编辑用户信息测试

---
+++ tests/pages/admin/users/id/edit.test.js
@@ -0,0 +1,66 @@
+import Form from '../../../../../pages/admin/users/[id]/edit';
+import {fireEvent, render, screen} from '@testing-library/react';
+import {MemoryRouter} from 'react-router';
+import React from 'react';
+import {app} from '@mxjs/app';
+import $ from 'miaoxing';
+import {bootstrap, createPromise, setUrl, resetUrl} from '@mxjs/test';
+
+bootstrap();
+
+describe('admin/users', () => {
+  beforeEach(() => {
+    setUrl('admin/users/1/edit');
+    app.page = {
+      collection: 'admin/users',
+      index: false,
+    };
+  });
+
+  afterEach(() => {
+    resetUrl();
+    app.page = {};
+  });
+
+  test('form', async () => {
+    const promise = createPromise();
+    const promise2 = createPromise();
+
+    $.http = jest.fn()
+      // 读取默认数据
+      .mockImplementationOnce(() => promise.resolve({
+        code: 1,
+        data: {
+          id: 1,
+          name: '姓名2',
+          mobile: '13800138000',
+          mobileVerifiedAt: '2020-01-01 00:00:00',
+        },
+      }))
+      // 提交
+      .mockImplementationOnce(() => promise2.resolve({
+        code: 1,
+      }));
+
+    const {container, getByLabelText, findByDisplayValue} = render(<MemoryRouter>
+      <Form/>
+    </MemoryRouter>);
+
+    await Promise.all([promise]);
+    expect($.http).toHaveBeenCalledTimes(1);
+    expect($.http).toMatchSnapshot();
+
+    // 看到表单加载了数据
+    await findByDisplayValue('姓名2');
+    await findByDisplayValue('13800138000');
+    expect(container.querySelector('#isMobileVerified').checked).toBeTruthy();
+
+    // 提交表单
+    fireEvent.change(getByLabelText('姓名'), {target: {value: '新名称'}});
+    fireEvent.click(screen.getByText('提 交'));
+
+    await Promise.all([promise2]);
+    expect($.http).toHaveBeenCalledTimes(2);
+    expect($.http).toMatchSnapshot();
+  });
+});
