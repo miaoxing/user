7a35a334c44fe58b5257da5f256c9f40b183df89
twinh <twinhuang@qq.com>
2020-09-22 16:57:50 +0800

    test: 测试修改用户信息

--- tests/pages/admin-api/users/IdTest.php
+++ tests/pages/admin-api/users/IdTest.php
@@ -33,4 +33,78 @@ class IdTest extends BaseTestCase
         $this->assertRetSuc($ret);
         $this->assertSame('测试', $ret['data']['name']);
     }
+
+    public function testPatch()
+    {
+        UserModel::findAllBy('mobile', '13800138001')->destroy();
+
+        $user = UserModel::save([
+            'name' => '测试',
+            'mobile' => '13800138000',
+        ]);
+        $this->assertNull($user->mobileVerifiedAt);
+
+        $ret = Tester::patchAdminApi('users/' . $user->id, [
+            'name' => '测试2',
+            'mobile' => '13800138001',
+            'isMobileVerified' => true,
+        ]);
+        $this->assertRetSuc($ret);
+
+        $user->reload();
+        $this->assertSame('测试2', $user->name);
+        $this->assertSame('13800138001', $user->mobile);
+        $this->assertNotEmpty($user->mobileVerifiedAt);
+    }
+
+    public function testPatchInvalidMobile()
+    {
+        $user = UserModel::save([
+            'name' => '测试',
+            'mobile' => '13800138000',
+        ]);
+
+        $ret = Tester::patchAdminApi('users/' . $user->id, [
+            'mobile' => '123',
+        ]);
+        $this->assertRetErr($ret, null, '手机必须是11位数字,以13到19开头');
+    }
+
+    public function testPatchMobileExists()
+    {
+        UserModel::findAllBy('mobile', '13800138001')->destroy();
+
+        UserModel::new()->setMobileVerified()->save([
+            'name' => '测试',
+            'mobile' => '13800138001',
+        ]);
+
+        $user2 = UserModel::save();
+
+        $ret = Tester::patchAdminApi('users/' . $user2->id, [
+            'mobile' => '13800138001',
+            'isMobileVerified' => true,
+        ]);
+        $this->assertRetErr($ret, null, '已存在认证该手机号码的用户');
+    }
+
+    public function testPatchCancelMobileVerify()
+    {
+        UserModel::findAllBy('mobile', '13800138001')->destroy();
+
+        $user = UserModel::new()->setMobileVerified()->save([
+            'name' => '测试',
+            'mobile' => '13800138001',
+        ]);
+
+        $ret = Tester::patchAdminApi('users/' . $user->id, [
+            'isMobileVerified' => false,
+        ]);
+        $this->assertRetSuc($ret);
+
+        $user->reload();
+        $this->assertSame('13800138001', $user->mobile);
+        // FIXME $user->isMobileVerified Property or object "isMobileVerified" (class "Wei\Validator\MobileVerified") not found
+        $this->assertFalse($user->getIsMobileVerifiedAttribute());
+    }
 }
