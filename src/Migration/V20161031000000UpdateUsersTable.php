59f3f7fd070364aa993f6ed070f366d395b1dd86
twinh <twinhuang@qq.com>
2020-11-30 15:45:16 +0800

    refactor: 升级数据库迁移语句

--- src/Migration/V20161031000000UpdateUsersTable.php
+++ src/Migration/V20161031000000UpdateUsersTable.php
@@ -15,9 +15,9 @@ class V20161031000000UpdateUsersTable extends BaseMigration
         $table
             ->char('wechat_open_id', 28)->comment('微信的OpenID')->after('group_id')
             ->char('wechat_union_id', 29)->after('wechat_open_id')
-            ->int('score')->comment('积分')
-            ->decimal('money', 16, 2)->comment('账户余额')
-            ->decimal('recharge_money', 16, 2)->comment('充值账户余额')
+            ->uInt('score')->comment('积分')
+            ->uDecimal('money', 16, 2)->comment('账户余额')
+            ->uDecimal('recharge_money', 16, 2)->comment('充值账户余额')
             ->bool('is_subscribed')->comment('是否关注')
             ->timestamp('subscribed_at')->comment('关注时间')
             ->timestamp('unsubscribed_at')->comment('取关时间')
