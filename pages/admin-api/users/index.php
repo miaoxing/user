9ca93c75248098ed3f65b3a1f6175faeb53f741d
twinh <twinhuang@qq.com>
2021-10-18 20:27:08 +0800

    style(user): php-cs-fixer

--- pages/admin-api/users/index.php
+++ pages/admin-api/users/index.php
@@ -3,6 +3,6 @@
 use Miaoxing\Plugin\BaseController;
 use Miaoxing\Services\Page\CollGetTrait;
 
-return new class extends BaseController {
+return new class() extends BaseController {
     use CollGetTrait;
 };
