72a7d4459e7880a01bd6d37564d11cbfc1a136fa
twinh <twinhuang@qq.com>
2023-08-16 18:33:06 +0800

    refactor: 简化引入的表单组件

--- pages/admin/users/[id]/edit.js
+++ pages/admin/users/[id]/edit.js
@@ -1,7 +1,7 @@
 import {CListBtn} from '@mxjs/a-clink';
 import {Page, PageActions} from '@mxjs/a-page';
 import {Form, FormItem, FormAction} from '@mxjs/a-form';
-import {Checkbox, Form as AntdForm} from 'antd';
+import {Checkbox} from 'antd';
 
 const Edit = () => {
   return (
@@ -13,9 +13,9 @@ const Edit = () => {
       <Form>
         <FormItem label="姓名" name="name"/>
         <FormItem label="手机" name="mobile" extra={
-          <AntdForm.Item name="isMobileVerified" valuePropName="checked">
+          <FormItem name="isMobileVerified" valuePropName="checked">
             <Checkbox>已验证</Checkbox>
-          </AntdForm.Item>
+          </FormItem>
         }/>
         <FormItem name="id" type="hidden"/>
         <FormAction/>
