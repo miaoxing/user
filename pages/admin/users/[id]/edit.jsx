59ee1bb95f67df356856a383d2fd5087f83ec8e1
twinh <twinhuang@qq.com>
2024-07-14 15:47:05 +0800

    feat(u): 调整页面内容为卡片视图

--- pages/admin/users/[id]/edit.jsx
+++ pages/admin/users/[id]/edit.jsx
@@ -1,7 +1,8 @@
-import {CListBtn} from '@mxjs/a-clink';
-import {Page, PageActions} from '@mxjs/a-page';
-import {Form, FormItem, FormAction} from '@mxjs/a-form';
-import {Checkbox} from 'antd';
+import { CListBtn } from '@mxjs/a-clink';
+import { Page, PageActions } from '@mxjs/a-page';
+import { Form, FormItem, FormAction } from '@mxjs/a-form';
+import { Checkbox } from 'antd';
+import { Section } from '@mxjs/a-section';
 
 const Edit = () => {
   return (
@@ -11,14 +12,16 @@ const Edit = () => {
       </PageActions>
 
       <Form>
-        <FormItem label="姓名" name="name"/>
-        <FormItem label="手机" name="mobile" extra={
-          <FormItem name="isMobileVerified" valuePropName="checked">
-            <Checkbox>已验证</Checkbox>
-          </FormItem>
-        }/>
-        <FormItem name="id" type="hidden"/>
-        <FormAction/>
+        <Section>
+          <FormItem label="姓名" name="name"/>
+          <FormItem label="手机" name="mobile" extra={
+            <FormItem name="isMobileVerified" valuePropName="checked" style={{marginBottom: 0}}>
+              <Checkbox>已验证</Checkbox>
+            </FormItem>
+          }/>
+        </Section>
+
+        <FormAction variant="card"/>
       </Form>
     </Page>
   );
